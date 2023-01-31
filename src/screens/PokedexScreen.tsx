9ae897aaf3ccd88ece7397c86a0d414a992d7e91
Hanzell Rivera <hanzellrivera95@gmail.com>
2023-01-31 10:46:21 -0600

    Added indicador activity loading

--- src/screens/PokedexScreen.tsx
+++ src/screens/PokedexScreen.tsx
@@ -1,13 +1,23 @@
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, ActivityIndicator, StyleSheet} from 'react-native';
 import React, {useContext} from 'react';
 import PokemonList from '../components/PokemonList';
 import {PokemonContext} from '../context/PokemonProvider';
 
 export default function PokedexScreen(): JSX.Element {
   const {pokemons} = useContext(PokemonContext);
-  return (
+  return pokemons.length > 0 ? (
     <SafeAreaView>
       <PokemonList pokemons={pokemons} />
     </SafeAreaView>
+  ) : (
+    <ActivityIndicator size={100} color="#AEAEAE" style={styles.spinner} />
   );
 }
+
+const styles = StyleSheet.create({
+  spinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
